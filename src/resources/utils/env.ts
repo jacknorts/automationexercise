09ddaaa1118f75273906bcea116426676047eba1
Jack Norton <sc18jcn@leeds.ac.uk>
2025-12-19 10:31:24 +0000

    Updated env.ts

--- src/resources/utils/env.ts
+++ src/resources/utils/env.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-export const env = {
+export const env = { // Not needed for this framework but kept for reference
     baseUrl: process.env.BASE_URL!,
     username: process.env.USERNAME!,
     password: process.env.PASSWORD!
